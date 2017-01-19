70dd2f6d3eade0ad8c41cca28164b6b0eec7cf46
movefast <xli@yelp.com>
2017-01-18 18:22:10 -0800

    rebuild gitignore

--- ParseStarterProject/src/main/java/com/parse/starter/UserFeedActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/UserFeedActivity.java
@@ -1,22 +1,31 @@
 package com.parse.starter;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+//import android.content.Intent;
+//import android.graphics.Bitmap;
+//import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.view.Menu;
+import android.view.MenuItem;
+//import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-import com.parse.FindCallback;
-import com.parse.GetDataCallback;
-import com.parse.ParseException;
-import com.parse.ParseFile;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-
-import java.util.List;
+import com.longtailvideo.jwplayer.JWPlayerFragment;
+import com.longtailvideo.jwplayer.JWPlayerView;
+import com.longtailvideo.jwplayer.media.playlists.PlaylistItem;
+//import android.widget.ImageView;
+//import android.widget.LinearLayout;
+//
+//import com.parse.FindCallback;
+//import com.parse.GetDataCallback;
+//import com.parse.ParseException;
+//import com.parse.ParseFile;
+//import com.parse.ParseObject;
+//import com.parse.ParseQuery;
+//
+//import java.util.List;
 
 public class UserFeedActivity extends AppCompatActivity {
 
@@ -26,67 +35,113 @@ public class UserFeedActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_feed);
 
 
-        final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+//        final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+//
+//        Intent intent = getIntent();
+//
+//        String activeUsername = intent.getStringExtra("username");
+//
+//        setTitle(activeUsername + "'s Feed");
+//
+//        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Image");
+//
+//        query.whereEqualTo("username", activeUsername);
+//        query.orderByDescending("createdAt");
+//
+//        query.findInBackground(new FindCallback<ParseObject>() {
+//            @Override
+//            public void done(List<ParseObject> objects, ParseException e) {
+//
+//                if (e == null) {
+//
+//                    if (objects.size() > 0) {
+//
+//                        for (ParseObject object : objects) {
+//
+//                            ParseFile file = (ParseFile) object.get("image");
+//
+//                            file.getDataInBackground(new GetDataCallback() {
+//                                @Override
+//                                public void done(byte[] data, ParseException e) {
+//
+//                                    if (e == null && data != null) {
+//
+//                                        Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+//
+//                                        ImageView imageView = new ImageView(getApplicationContext());
+//
+//                                        imageView.setLayoutParams(new ViewGroup.LayoutParams(
+//                                                ViewGroup.LayoutParams.MATCH_PARENT,
+//                                                ViewGroup.LayoutParams.WRAP_CONTENT
+//                                        ));
+//
+//                                        imageView.setImageBitmap(bitmap);
+//
+//                                        linearLayout.addView(imageView);
+//
+//                                    }
+//
+//
+//                                }
+//                            });
+//
+//                        }
+//
+//                    }
+//
+//                }
+//
+//            }
+//        });
 
-        Intent intent = getIntent();
+//        String frameVideo = "<html><body>Video From YouTube<br><iframe src=\"https://content.jwplatform.com/players/3zGF1Ub9-OEdF1fhP.html\" width=\"480\" height=\"270\" frameborder=\"0\" scrolling=\"auto\" allowfullscreen></body></html>";
+//        WebView displayYoutubeVideo = (WebView) findViewById(R.id.myWebView);
+//        displayYoutubeVideo.setWebViewClient(new WebViewClient() {
+//            @Override
+//            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//                return false;
+//            }
+//        });
+//        WebSettings webSettings = displayYoutubeVideo.getSettings();
+//        webSettings.setJavaScriptEnabled(true);
+//        displayYoutubeVideo.loadData(frameVideo, "text/html", "utf-8");
 
-        String activeUsername = intent.getStringExtra("username");
+        // Get a handle to the JWPlayerFragment
+        JWPlayerFragment fragment = (JWPlayerFragment) getFragmentManager().findFragmentById(R.id.playerFragment);
 
-        setTitle(activeUsername + "'s Feed");
+        // Get a handle to the JWPlayerView
+        JWPlayerView playerView = fragment.getPlayer();
 
-        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Image");
+        // Create a PlaylistItem
+        PlaylistItem video = new PlaylistItem("http://content.jwplatform.com/videos/3zGF1Ub9-YgY5iOVA.mp4");
 
-        query.whereEqualTo("username", activeUsername);
-        query.orderByDescending("createdAt");
+        // Load a stream into the player
 
-        query.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> objects, ParseException e) {
-
-                if (e == null) {
-
-                    if (objects.size() > 0) {
-
-                        for (ParseObject object : objects) {
-
-                            ParseFile file = (ParseFile) object.get("image");
-
-                            file.getDataInBackground(new GetDataCallback() {
-                                @Override
-                                public void done(byte[] data, ParseException e) {
-
-                                    if (e == null && data != null) {
-
-                                        Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-
-                                        ImageView imageView = new ImageView(getApplicationContext());
-
-                                        imageView.setLayoutParams(new ViewGroup.LayoutParams(
-                                                ViewGroup.LayoutParams.MATCH_PARENT,
-                                                ViewGroup.LayoutParams.WRAP_CONTENT
-                                        ));
-
-                                        imageView.setImageBitmap(bitmap);
-
-                                        linearLayout.addView(imageView);
-
-                                    }
-
-
-                                }
-                            });
-
-                        }
-
-                    }
-
-                }
-
-            }
-        });
-
+        playerView.load(video);
 
 
 
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
